eb432baf3d6ef8c6f2edf8be9a92e6f2a65bae46
rpmaya <35469068+rpmaya@users.noreply.github.com>
2024-10-21 18:44:09 +0200

    Update users.js

--- models/mysql/users.js
+++ models/mysql/users.js
@@ -12,7 +12,9 @@ const User = sequelize.define(
             type: DataTypes.INTEGER
         },
         email: {
-            type: DataTypes.STRING
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
         },
         password:{
             type: DataTypes.STRING
